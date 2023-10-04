b39564e78314044d018b4fd2a351f7b6ded2b36a
Kyonyx12 <themisteryofworld@gmail.com>
2023-10-03 18:45:46 -0700

    Segunda practica con javascrip, operadores logicos

---
+++ js_practica2/js/script.js
@@ -0,0 +1,20 @@
+var valores = [true,10,false,"codigo","herramientas",7];
+
+if(valores[3]>valores[4]){
+    alert("El texto mayor es: "+valores[3]);
+} else {
+    alert("El texto mayor es: "+valores[4]);
+}
+
+alert(valores[0] != valores[2]);
+alert(valores[0] == valores[2]);
+
+var suma = valores[1] + valores[5];
+var resta = valores[1] - valores[5];
+var multi = valores[1] * valores[5];
+var divi = valores[1] / valores[5];
+var modulo =  valores[1] % valores[5];
+
+alert("La suma de los numeros es: "+suma+" \nLa resta de los numeros es: "+resta+" \nLa multiplicacion de los numeros es: "+multi+" \nLa division de los numeros es: "+divi+" \nEl modulo de los numeros es: "+modulo);
+
+//Jose Antonio Lopez Ramos - Grupo 372
